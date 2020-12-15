cac8a9f4d0dd190e2dbb2f1fb36006de4bfb895b
michalx5 <hajzl@poass.cz>
2020-12-15 17:39:52 +0100

    Add project files.

---
+++ marekt/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace marekt.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public DbSet<Child> Children { get; set; }
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
